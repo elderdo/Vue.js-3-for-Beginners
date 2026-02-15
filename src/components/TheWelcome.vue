050476a75cc2e7fa064c00e291e4321cdcfe44ac
elderdo <elderdo@yahoo.com>
2026-02-14 18:02:16 -0600

    Refactor SocialPost component to use props for dynamic data and update TheWelcome.vue to display multiple posts

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,6 +1,38 @@
 <script setup>
+  import { reactive } from 'vue';
   import SocialPost from './molecules/SocialPost.vue';
+  const posts = reactive([
+    { username: "Username one",
+      userId: "usernameId1",
+      avatarSrc: "https://i.pravatar.cc/40",
+      post:"This is post 1"
+    },
+    { username: "Username two",
+      userId: "usernameId2",
+      avatarSrc: "https://i.pravatar.cc/41",
+      post:"This is post 2"
+    },
+    { username: "Username three",
+      userId: "usernameId3",
+      avatarSrc: "https://i.pravatar.cc/42",
+      post:"This is post 3"
+    },
+  ]);
 </script>
 <template>
-  <SocialPost></SocialPost>
+  <SocialPost
+    :username="posts[0].username"
+    :userId="posts[0].userId"
+    :avatarSrc="posts[0].avatarSrc"
+    :post="posts[0].post"></SocialPost>
+  <SocialPost
+    :username="posts[1].username"
+    :userId="posts[1].userId"
+    :avatarSrc="posts[1].avatarSrc"
+    :post="posts[1].post"></SocialPost>
+  <SocialPost
+    :username="posts[2].username"
+    :userId="posts[2].userId"
+    :avatarSrc="posts[2].avatarSrc"
+    :post="posts[2].post"></SocialPost>
 </template>
--- src/components/molecules/SocialPost.vue
+++ src/components/molecules/SocialPost.vue
@@ -1,17 +1,34 @@
 <template>
-    <div class="SocialPost">
+    <div class="SocialPost"
+        :class="{ SocialPost__selected: selected}"
+        @click="selected = !selected"
+        >
         <div class="header">
-            <img class="avatar" src="https://i.pra-vatar.cc/40"/>
-            <div class="name">Name of User</div>
-            <div class="userId">@userId</div>
+            <img class="avatar" :src="avatarSrc"/>
+            <div class="name">{{ username }}</div>
+            <div class="userId">{{ userId }}</div>
         </div>
-        <div class="post">This is a dummy post</div>
+        <div class="post">{{ post }}</div>
     </div>
 </template>
 <script setup>
+    import { onMounted, ref } from 'vue';
+    const selected = ref(false);
+    const props = defineProps({
+       username: String,
+       userId: String,
+       avatarSrc: String,
+       post: String
+    });
+    onMounted( () => {
+        console.log(props.username);
+    });
 </script>
 <style lang="scss">
 .SocialPost {
+    &__selected {
+        border: white solid 1px;
+    }
     .header {
         display: flex;
         align-items: center;
